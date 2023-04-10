d30a37c17411488c8a251a4e08eb850f8ed38d9d
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-04-10 19:00:54 -0400

    formatting

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -1,8 +1,7 @@
-
 const phrases = [
-    'A Junior Full-Stack Developer',
-    'A Computer Science Student',
-    'Based On The East Coast',
+  'A Junior Full-Stack Developer',
+  'A Computer Science Student',
+  'Based On The East Coast',
 ];
 const element = document.getElementById('typewriter');
 
@@ -11,41 +10,40 @@ let index = 0;
 let word;
 
 const writeLoop = async () => {
-    // Sleeps on initial load
-    await sleep(sleepTime * 10);
-    
-    while (true) {
-        word = phrases[index];    
+  // Sleeps on initial load
+  await sleep(sleepTime * 10);
 
-        for (let i = 0; i < word.length; i++) {
-            element.innerText = word.substring(0, i + 1);
-            // Speed of typed chars
-            await sleep(sleepTime);
-        }
+  while (true) {
+    word = phrases[index];
 
-        // While full await
-        await sleep(sleepTime * 30)
-        
-        for (let i = word.length; i > 0; i--) {
-            element.innerText = word.substring(0, i - 1);
-            // Speed of deleted chars
-            await sleep(sleepTime / 1.5);
-        }
-
-        if (index === phrases.length - 1) {
-            index = 0;
-        }
-        else {
-            index ++;
-        }
-
-        // While empty await 
-        await sleep(sleepTime * 12);
+    for (let i = 0; i < word.length; i++) {
+      element.innerText = word.substring(0, i + 1);
+      // Speed of typed chars
+      await sleep(sleepTime);
     }
+
+    // While full await
+    await sleep(sleepTime * 30)
+
+    for (let i = word.length; i > 0; i--) {
+      element.innerText = word.substring(0, i - 1);
+      // Speed of deleted chars
+      await sleep(sleepTime / 1.5);
+    }
+
+    if (index === phrases.length - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+
+    // While empty await 
+    await sleep(sleepTime * 12);
+  }
 }
 
 function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 writeLoop();
