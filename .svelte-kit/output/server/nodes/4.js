0bad15fc19c59d02a1ee68812501ba4bef5cd474
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-07-06 19:43:51 -0400

    removed .svelte-kit tracking

--- .svelte-kit/output/server/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/contact/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.343247c0.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.67206439.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/5.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 5;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/projects/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/5.343247c0.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.67206439.js"];
-export const stylesheets = [];
-export const fonts = [];
