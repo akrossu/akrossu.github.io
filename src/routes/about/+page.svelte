9b69d6dadf40424bd3102f4a3f6d15f9970fe3d9
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-07-07 16:50:51 -0400

    added contact links

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+    import Links from "../../components/links.svelte";
+
+    const imageStyle = ''
+</script>
+
 <main class="about">
     <div class="about mx-auto max-w-7xl lg:px-8">
         <div class="relative px-4 sm:px-8 lg:px-12">
@@ -6,7 +12,7 @@
                     <h1 class="font-mono max-w-xl text-5xl font-semibold leading-[3.5rem]">
                         I'm Zach Merritt.
                     </h1>
-                    <div class="xl:grid xl:grid-cols-2">
+                    <div class="lg:grid lg:grid-cols-2">
                         <div>
                             <br />
                             <p>
@@ -20,11 +26,34 @@
                                 I am currently a Computer Science student and look for every opportunity while still in school
                                 to develop my professional experience and collaborate with others.
                             </p><br />
-                            <!-- TODO: make paragraph max width fit to content section -->
                         </div>
-                        <img class="hidden xl:inline-block max-w-lg mt-12 ml-8 border border-zinc-700 rounded-lg" src="/img/About.png" alt="Code">
+                        <div class="place-self-center justify-self-center">
+                            <img class="hidden lg:inline-block w-64 object-none object-center border border-zinc-700 rounded-lg" src="/img/pfp.jpg" alt="Code">
+                            <div class="mt-8">
+                                <ul role="list">
+                                    <hr class="block lg:hidden mt-4 mb-8 bg-neutral-700 border-0 h-px">
+                                    <li class="">
+                                        <Links website="github" text="Follow on GitHub" style="h-8 w-8" />
+                                    </li>
+                                    <li class="mt-4 flex">
+                                        <Links website="linkedin" text="Follow on LinkedIn" style="h-8 w-8" />
+                                    </li>
+                                    <li class="mt-4 flex">
+                                        <Links website="discord" text="Add on Discord" style="h-8 w-8" />
+                                    </li>
+                                    <hr class="hidden lg:block my-4 bg-neutral-700 border-0 h-px">
+                                    <li class="flex">
+                                        <a class="group flex mt-4 lg:mt-0" href="mailto:ZacharyT.Merritt@gmail.com">
+                                            <svg viewBox="0 0 24 24" aria-hidden="true" class="h-8 w-8 dark:fill-zinc-400 group-hover:fill-[#2d9248]">
+                                                <path fill-rule="evenodd" d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"></path></svg>
+                                            <span class="ml-4">ZacharyT.Merritt@gmail.com</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                <!-- </div> -->
+                </div>
             </div>
         </div>
     </div>
