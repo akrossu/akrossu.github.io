c3bb87c870cc52f2a49613581fb6eb60747ae0f4
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-07-07 16:50:03 -0400

    Updated import naming

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,11 +1,11 @@
 <script>
-    import Links from './__pages.svelte';
+    import Pages from './__pages.svelte';
 </script>
 
 <header class="flex justify-center">
     <nav class="relative sm:top-6">
         <ul class="flex sm:space-x-7 space-x-4 rounded-full py-[.55rem] sm:px-7 px-4 sm:text-base text-sm bg-zinc-800 border-zinc-700 border">
-            <Links />
+            <Pages />
         </ul>
     </nav>
     <div class="sm:h-32 h-16"></div>
