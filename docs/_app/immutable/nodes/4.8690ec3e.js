defeaf5c3b0408688f8c7ccb7f913872ce5fcdc1
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-07-07 17:16:42 -0400

    Update v0.2

--- docs/_app/immutable/nodes/4.8690ec3e.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.a7e64d23.js";import{S as t,i as e}from"../chunks/index.b43a879d.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
