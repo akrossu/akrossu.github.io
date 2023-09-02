314d71dd70f433f9621f247c362d9b81a3206552
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-09-01 23:32:54 -0400

    updated image

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,14 +5,14 @@
 </script>
 
 <svelte:head>
-    <title>About - Zach Merritt</title>    
+    <title>About - Zach</title>    
 </svelte:head>
 <main class="about">
     <div class="mx-auto max-w-7xl lg:px-8">
         <div class="relative px-4 sm:px-8 lg:px-12">
             <div class="mx-auto max-w-2xl lg:max-w-5xl">
                 <h1 class="font-mono max-w-xl sm:text-5xl text-3xl font-semibold leading-[3.5rem]">
-                    I'm Zach Merritt
+                    I'm Zach.
                 </h1>
                 <div class="lg:grid lg:grid-cols-2">
                     <div>
@@ -30,7 +30,7 @@
                         </p><br />
                     </div>
                     <div class="place-self-center justify-self-center">
-                        <img class="hidden lg:inline-block w-64 object-none object-center border border-zinc-700 rounded-lg" src="/img/pfp.jpg" alt="Code">
+                        <img class="hidden lg:inline-block w-64 object-center border border-zinc-700 rounded-lg" src="/img/pfp.png" alt="Code">
                         <div class="mt-8">
                             <ul role="list">
                                 <hr class="block lg:hidden mt-4 mb-8 bg-neutral-700 border-0 h-px">
@@ -43,14 +43,16 @@
                                 <li class="mt-4 flex relative">
                                     <Links website="discord" text="Add on Discord" style="h-6 w-6" />
                                 </li>
+                                <!-- 
                                 <hr class="hidden lg:block my-4 bg-neutral-700 border-0 h-px">
                                 <li class="flex relative">
                                     <a class="group flex mt-4 lg:mt-0" href="mailto:ZacharyT.Merritt@gmail.com">
                                         <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 dark:fill-zinc-400 group-hover:fill-[#2d9248]">
                                             <path fill-rule="evenodd" d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"></path></svg>
-                                        <span class="ml-4">ZacharyT.Merritt@gmail.com</span>
+                                        <span class="ml-4">Emails are open!</span>
                                     </a>
                                 </li>
+                                -->
                             </ul>
                         </div>
                     </div>
