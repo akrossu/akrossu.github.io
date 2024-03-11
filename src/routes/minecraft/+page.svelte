406b3a68bdebaec415016d3723c6660f6dcd048a
Akr0ss <zachary192837465@gmail.com>
2024-03-11 00:36:50 -0400

    Updated mod listing

--- src/routes/minecraft/+page.svelte
+++ src/routes/minecraft/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import StaticBars from '../../components/staticBars.svelte'
 
-    import img from '$lib/assets/darkrpg.png';
+    import img from '$lib/assets/medieval.png';
 
     function discordLinks() {
         const link1 = 'https://cdn.discordapp.com/attachments/678347708238659624/1216206376934834337/DistantHorizons-fabric-2.0.2-a-dev-1.20.1.jar?ex=65ff8b74&is=65ed1674&hm=c3563b8b6877dd64428279c092b9af88e9b603a18a6879c594d4eedd357efed6&';
@@ -13,31 +13,36 @@
 </script>
 
 <svelte:head>
-    <title>[ Home@Akross:/ ]</title>
+    <title>[ Home@Akross:/Minecraft ]</title>
 </svelte:head>
 
-<main class="select-none">
-    <StaticBars color1="bg-orange" color2="bg-red" color3="bg-magenta"></StaticBars>
+<main class="mb-[90px]">
+    <div class="hidden md:block">
+        <StaticBars color1="bg-orange" color2="bg-red" color3="bg-magenta"></StaticBars>
+    </div>
     <div class="mt-6">
-        <a href="/" class="relative ml-32 font-DinkieBitmap hover:text-yellow">&lt; Return home</a>
+        <a href="/" class="relative ml-32 font-DinkieBitmap text-2xl hover:text-yellow">&lt; Return home</a>
         <h1 class="text-5xl text-center pt-6 font-Zpix">Minecraft Modpacks</h1>
     </div>
-    <p class="my-16 w-[80%] mx-auto"></p>
-    <div class="mx-56">
-        <!-- <h2 class="font-DinkieBitmap text-3xl">Dark RPG</h2> -->
-        <div class="flex flex-row gap-6">
-            <img src={img} alt="" class="w-52 h-52">
+    <p class="my-16 mx-auto"></p>
+    <div class="mx-6 md:mx-24 lg:mx-[15%]">
+        <div class="block lg:flex lg:flex-row gap-6">
+            <img src={img} alt="" class="w-52 h-52 rounded-xl hidden lg:block">
             <div class="basis-3/4">
-                <h3 class="text-2xl font-DinkieBitmap">Steps</h3>
+                <h3 class="text-2xl font-DinkieBitmap">Medieval Modpack installatoin steps:</h3>
                 <ol class="font-Zpix">
-                    <li>- Install the <a href="https://download.overwolf.com/install/Download?ExtensionId=cfiahnpaolfnlgaihhmobmnjdafknjnjdpdabpcm" target="_blank" class="text-yellow hover:text-yellow-orange">CurseForge App</a> and the <a href="https://www.curseforge.com/minecraft/modpacks/darkrpg" target="_blank" class="text-yellow hover:text-yellow-orange">Dark RPG modpack</a>.</li>
-                    <li>- Make sure to allocate &gt;8 gigabytes at LEAST.</li>
+                    <li>- Install the <a href="https://download.overwolf.com/install/Download?ExtensionId=cfiahnpaolfnlgaihhmobmnjdafknjnjdpdabpcm" target="_blank" class="text-yellow hover:text-yellow-orange">CurseForge App</a> and the <a href="https://www.curseforge.com/minecraft/modpacks/medieval-mc-fabric-mmc2" target="_blank" class="text-yellow hover:text-yellow-orange">Medieval Modpack</a>.</li>
+                    <li>- Make sure to allocate 8 - 10 gigabytes of RAM.</li>
+                    <li>- Delete and replace your mods folder with the <a href="https://drive.google.com/drive/folders/13oMCzweGkASWCXZzN_z40Fol3UIWnU0V" target="_blank" class="text-yellow hover:text-yellow-orange">updated mod list</a>.</li>
                 </ol>
-                <h3 class="text-2xl font-DinkieBitmap pt-4">(Optional)</h3>
+                <h4 class="text-xl font-DinkieBitmap pt-4">(Optional)</h4>
                 <ol class="font-Zpix">
-                    <li>Distant Horizons is a mod that allows you to substantially increase your render distance and is highly recommended to use with shaders.</li>
+                    <li>- You can <a href="https://cdn.discordapp.com/attachments/324008937705963521/1216605052182069330/config.zip?ex=6600febf&is=65ee89bf&hm=71c671b00d3a8b55dfbdd8b0638625b3129aaec0c1321efd0dffb2f39bbd9c42&" class="text-yellow hover:text-yellow-orange">download</a> my preset binds, shaders, and settings for an even easier experience.</li>
+                    <!-- Distant Horizons -->
+                    <li class="pt-2">Distant Horizons is a mod that allows you to substantially increase your render distance and is highly recommended to use with shaders.</li>
                     <li>- Download <button on:click={discordLinks} class="text-yellow hover:text-yellow-orange">these two mods</button> and place them within your mods folder.</li>
                     <li>- You will need <a href="https://github.com/X0nk/Bliss-Shader/archive/refs/heads/main.zip" class="text-yellow hover:text-yellow-orange">these specific shaders</a> in order for them to work with Distant Horizons.</li>
+                    <li>- Additionally, you can <a href="https://drive.google.com/file/d/1zopsMRsHw3la3BN1zaN4xUBV-9l0sVNV/view?usp=drive_link" class="text-yellow hover:text-yellow-orange">download</a> a large pregenerated area so the experience is seamless.</li>
                 </ol>
             </div>
             <div class="basis-1/4">
