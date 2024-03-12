7c8ed42d93ae14c5c8d0e864c0bd3d659b272ba5
Akr0ss <zachary192837465@gmail.com>
2024-03-12 15:10:00 -0400

    Added mobile support

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -10,16 +10,16 @@
     let showMinecraft:boolean;
 </script>
 
-<header class="font-DinkieBitmap text-2xl">
+<header class="font-DinkieBitmap text-2xl absolute">
     <nav class="ml-32 mt-6">
-        <ul class="flex col-auto gap-12">
-            <li class="inline-block relative" bind:this={directories}>
+        <ul class="md:flex col-auto gap-12">
+            <li class="md:inline-block" bind:this={directories}>
                 <button class="group" type="button" on:click={() => (directories = !directories)} aria-expanded="true" aria-haspopup="true">
                     [ <span class="group-hover:text-yellow {!directories ? "text-orange group-hover:text-yellow-orange": "text-white"}">directories</span> ]
                     <span class="inline-flex {!directories ? "scale-y-[-1] text-orange": "text-white"}">^</span>
                 </button>
                 {#if !directories}
-                <div class="origin-top-right absolute [&>*]:block ms-4">
+                <div class="origin-top-right md:absolute [&>*]:block ms-4">
                     <a href="https://kumiko.vercel.app/" class="hover:text-yellow" target="_blank">/ Kumiko.app</a>
                     <a href="https://discord.gg/sczXWkDsZ6" class="hover:text-yellow" target="_blank">/ Discord</a>
                     <a href="/minecraft" class="hover:text-yellow">/ Minecraft</a>
@@ -27,13 +27,13 @@
                 </div>
                 {/if}
             </li>
-            <li class="inline-block" bind:this={projects}>
+            <li class="md:inline-block" bind:this={projects}>
                 <button class="group" type="button" on:click={() => (projects = !projects)} aria-expanded="true" aria-haspopup="true">
                     [ <span class="group-hover:text-yellow {!projects ? "text-orange group-hover:text-yellow-orange": "text-white"}">projects</span> ]
                     <span class="inline-flex {!projects ? "scale-y-[-1] text-orange": "text-white"}">^</span>
                 </button>
                 {#if !projects}
-                <div class="origin-top-right absolute [&>*]:block ms-4">
+                <div class="origin-top-right md:absolute [&>*]:block ms-4">
                     <a href="https://github.com/akr0ss/kumiko-discord-bot" class="hover:text-yellow" target="_blank">/ Kumiko</a>
                     <a href="https://github.com/AKR0SS/Taki-MAL-API-Wrapper" class="hover:text-yellow" target="_blank">/ Taki</a>
                     <a href="https://github.com/AKR0SS/TimelyWatches" class="hover:text-yellow" target="_blank">/ TimelyWatches</a>
