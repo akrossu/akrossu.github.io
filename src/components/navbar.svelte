155590bdad7517f57c585336c42d35a8a55d8c1b
Akr0ss <zachary192837465@gmail.com>
2024-05-30 11:51:49 -0400

    removed unused pages

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -7,11 +7,10 @@
 
     let showContact:boolean;
     let showAbout:boolean;
-    let showMinecraft:boolean;
 </script>
 
-<header class="font-DinkieBitmap text-2xl absolute">
-    <nav class="ml-32 mt-6">
+<header class="flex font-DinkieBitmap text-2xl">
+    <nav class="mx-auto justify-center md:ml-32 mt-12 md:mt-6">
         <ul class="md:flex col-auto gap-12">
             <li class="md:inline-block" bind:this={directories}>
                 <button class="group" type="button" on:click={() => (directories = !directories)} aria-expanded="true" aria-haspopup="true">
@@ -23,8 +22,7 @@
                     <a href="https://kumiko.vercel.app/" class="hover:text-yellow" target="_blank">/ Kumiko.app</a>
                     <a href="https://discord.gg/sczXWkDsZ6" class="hover:text-yellow" target="_blank">/ Discord</a>
                     <a href="/minecraft" class="hover:text-yellow">/ Minecraft</a>
-                    <!-- <li><button on:click={() => (showMinecraft = !showMinecraft)}>[ <span class="hover:text-yellow">/ Minecraft</span> ]</button></li> -->
-                </div>
+                    </div>
                 {/if}
             </li>
             <li class="md:inline-block" bind:this={projects}>
@@ -36,7 +34,6 @@
                 <div class="origin-top-right md:absolute [&>*]:block ms-4">
                     <a href="https://github.com/akr0ss/kumiko-discord-bot" class="hover:text-yellow" target="_blank">/ Kumiko</a>
                     <a href="https://github.com/AKR0SS/Taki-MAL-API-Wrapper" class="hover:text-yellow" target="_blank">/ Taki</a>
-                    <a href="https://github.com/AKR0SS/TimelyWatches" class="hover:text-yellow" target="_blank">/ TimelyWatches</a>
                 </div>
                 {/if}
             </li>
