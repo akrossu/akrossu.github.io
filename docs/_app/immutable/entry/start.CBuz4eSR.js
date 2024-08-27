1aae904581ce5e404fb34d0fb638c37e2956e128
Akross <59298840+akrossu@users.noreply.github.com>
2024-08-27 00:04:54 -0400

    v0.7.2
    Rewrote some of the text to fit my identity crisis better

---
+++ docs/_app/immutable/entry/start.CBuz4eSR.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BP9GFhlm.js";export{t as start};
---
+++ docs/_app/immutable/nodes/11.Ba3Xi9Bt.js
@@ -0,0 +1,14 @@
+import{s as V,a as u,e as _,h as F,d as v,c as p,b as x,f as K,g as C,i as h,j as E,k as r}from"../chunks/scheduler.BHOCWXDt.js";import{S as M,i as N,c as q,a as A,m as H,t as G,b as T,d as D}from"../chunks/index.D1hKqEVN.js";import{S as O,H as Q}from"../chunks/sidebar.DZr3MBZb.js";function U(R){let m,b,o,g,e,l,$,d,S="> thank you for visiting!! feel free to leave your mark here for others to find your site and have a nice journey :3",w,i,B="// please be courteous and kind, comments are manually approved.",k,a,I="> loading comments...",j,s,P=`if(!window.hcb_user) { hcb_user={}; }\r
+        hcb_user.PAGE="https://akross.dev/";\r
+        (function() {\r
+            var s = document.createElement("script"),\r
+            l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"),\r
+            h="https://www.htmlcommentbox.com";s.setAttribute("type","text/javascript");\r
+            s.setAttribute("src", h + "/jread?page=" + encodeURIComponent(l).replace("+","%2B") + \r
+                "&mod=%241%24wq1rdBcg%24hkIBO3klmY8h3KTfjFGwQ." + "&opts=17558&num=10&ts=1723579112442");\r
+            if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})\r
+        ();\r
+\r
+        // onMount(() => {\r
+        //     document.querySelector('admin-link').style.display = 'none';\r
+        // });`,f;return document.title=m="[ home@akross:/"+y+" ]",o=new O({props:{title:y}}),l=new Q({props:{title:y}}),{c(){b=u(),q(o.$$.fragment),g=u(),e=_("div"),q(l.$$.fragment),$=u(),d=_("p"),d.textContent=S,w=u(),i=_("h3"),i.textContent=B,k=u(),a=_("div"),a.textContent=I,j=u(),s=_("script"),s.textContent=P,this.h()},l(t){F("svelte-1x1mgtu",document.head).forEach(v),b=p(t),A(o.$$.fragment,t),g=p(t),e=x(t,"DIV",{class:!0});var n=K(e);A(l.$$.fragment,n),$=p(n),d=x(n,"P",{"data-svelte-h":!0}),C(d)!=="svelte-1lq3l4c"&&(d.textContent=S),w=p(n),i=x(n,"H3",{class:!0,"data-svelte-h":!0}),C(i)!=="svelte-7briai"&&(i.textContent=B),k=p(n),a=x(n,"DIV",{id:!0,class:!0,"data-svelte-h":!0}),C(a)!=="svelte-101kg08"&&(a.textContent=I),j=p(n),s=x(n,"SCRIPT",{type:!0,id:!0,"data-svelte-h":!0}),C(s)!=="svelte-1e4b6ba"&&(s.textContent=P),n.forEach(v),this.h()},h(){h(i,"class","mb-4"),h(a,"id","HCB_comment_box"),h(a,"class","text-text svelte-1u5fl6j"),h(s,"type","text/javascript"),h(s,"id","hcb"),h(e,"class","max-w-content w-full")},m(t,c){E(t,b,c),H(o,t,c),E(t,g,c),E(t,e,c),H(l,e,null),r(e,$),r(e,d),r(e,w),r(e,i),r(e,k),r(e,a),r(e,j),r(e,s),f=!0},p(t,[c]){(!f||c&0)&&m!==(m="[ home@akross:/"+y+" ]")&&(document.title=m)},i(t){f||(G(o.$$.fragment,t),G(l.$$.fragment,t),f=!0)},o(t){T(o.$$.fragment,t),T(l.$$.fragment,t),f=!1},d(t){t&&(v(b),v(g),v(e)),D(o,t),D(l)}}}let y="guestbook";class L extends M{constructor(m){super(),N(this,m,null,U,V,{})}}export{L as component};
