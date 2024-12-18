62c6efccf5f08c4a5850b6ab4f6ff1ae86fa8841
Akross <59298840+akrossu@users.noreply.github.com>
2024-12-18 01:29:11 -0500

    updated list

--- src/routes/game-list/+page.svelte
+++ src/routes/game-list/+page.svelte
@@ -13,32 +13,26 @@
 <div class="max-w-content w-full">
     <Header title={title}></Header>
     <!-- Content -->
-    <p>&gt Here's a list of all of my currently playing, completed, and plan to plays!</p>
+    <p>&gt Here's a list of all of my currently playing, completed, and plan to plays</p>
     
-    <h2 class="mt-4">Playing</h2>
+    <h2 class="mt-4">what i'm playing</h2>
     <div class="ps-4">
-        <p>Current: Elden Ring</p>
-        <p>secondary: </p>
-        <p>backburner: Stalker GAMMA</p>
+        <p>&gt current: Dragon's Dogma 2</p>
+        <p>&gt secondary: Path of Exile 2 &gt;:3</p>
     </div>
 
     <h2 class="mt-4">Plan to play games</h2>
     <h3>// Short and cute games ( &lt 10 hours )</h3>
     <ul class="ps-4">
+        <li>&gt firewatch</li>
         <li>&gt road 96</li>
         <li>&gt a short hike</li>
         <li>&gt Before Your Eyes</li>
-        <li>&gt stray</li>
-        <li>&gt fnaf: into the pit</li>
-        <li>&gt VLADiK BRUTAL</li>
-        <li>&gt the mortuary assistant</li>
     </ul>
     <h3>// medium length games ( 10 - 40 hours )</h3>
     <ul class="ps-4">
         <li>&gt half-life alyx</li>
-        <li>&gt final fantasy 10</li>
-        <li>&gt still wakes the deep</li>
-        <li>&gt nine sols</li>
+        <li>&gt Silent Hill 2 remake</li>
     </ul>
     <h3>// longggg games ( &gt 40 hours )</h3>
     <ul class="ps-4">
@@ -47,9 +41,13 @@
         <li>&gt persona series pfffffft</li>
     </ul>
 
-    <h2 class="mt-4">completed</h2>
+    <div>
+        <h2 class="mt-4 inline-flex">completed games</h2>
+        <h3 class="inline-flex">(perchance some reviews)</h3>
+    </div>
+    <p class="pb-2">I'll probably move this once there's more reviews to the community page</p>
     <h3 class="inline">Hazy Mind </h3><span>( 8/10 )</span>
     <p class="ps-4">&gt an indie phsycological horror game that was honestly really hard to get through since it is based on the authors struggle with their diagnosis and mental health issues. With the multiple endings and feeling of genuine care and story telling it kept me at the edge of my seat and i loved it.</p>
     <h3 class="inline">Neon White </h3><span>( 9/10 )</span>
-    <p class="ps-4">dude. this game is seriously so good. literally the speedrunner-esc playstyle is addictive and then the <span class="normal-case">STORY</span> oml it's so devistating and so sad but so good and it's easily my favorite game i've played as of recent- end of run on sentence.</p>
+    <p class="ps-4">&gt dude. this game is seriously so good. literally the speedrunner-esc playstyle is addictive and then the <span class="normal-case">STORY</span> oml it's so devistating and so sad but so good and it's easily my favorite game i've played as of recent- end of run on sentence.</p>
 </div>
