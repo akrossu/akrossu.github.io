7a3b024a6244ceb62e3e3fd549fcef51031bc31d
Akross <59298840+akrossu@users.noreply.github.com>
2024-12-18 02:52:37 -0500

    v8.1
    Updated API call caching

---
+++ docs/_app/immutable/nodes/11.Bmx-8u2K.js
@@ -0,0 +1,14 @@
+import{s as V,a as u,f as _,z,d as v,c as p,g as x,h as F,y,j as h,i as E,A as r}from"../chunks/scheduler.BtYfPFkB.js";import{S as K,i as M,b as P,d as q,m as H,a as G,t as T,e as D}from"../chunks/index.D_F554_W.js";import{S as N,H as O}from"../chunks/sidebar.DryN8hsx.js";function Q(R){let m,b,o,g,e,l,$,d,S="> thank you for visiting!! feel free to leave your mark here for others to find your site and have a nice journey :3",w,i,A="// please be courteous and kind, comments are manually approved.",k,a,B="> loading comments...",j,s,I=`if(!window.hcb_user) { hcb_user={}; }\r
+        hcb_user.PAGE="https://akross.dev/";\r
+        (function() {\r
+            var s = document.createElement("script"),\r
+            l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"),\r
+            h="https://www.htmlcommentbox.com";s.setAttribute("type","text/javascript");\r
+            s.setAttribute("src", h + "/jread?page=" + encodeURIComponent(l).replace("+","%2B") + \r
+                "&mod=%241%24wq1rdBcg%24hkIBO3klmY8h3KTfjFGwQ." + "&opts=17558&num=10&ts=1723579112442");\r
+            if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})\r
+        ();\r
+\r
+        // onMount(() => {\r
+        //     document.querySelector('admin-link').style.display = 'none';\r
+        // });`,f;return document.title=m="[ home@akross:/"+C+" ]",o=new N({props:{title:C}}),l=new O({props:{title:C}}),{c(){b=u(),P(o.$$.fragment),g=u(),e=_("div"),P(l.$$.fragment),$=u(),d=_("p"),d.textContent=S,w=u(),i=_("h3"),i.textContent=A,k=u(),a=_("div"),a.textContent=B,j=u(),s=_("script"),s.textContent=I,this.h()},l(t){z("svelte-1x1mgtu",document.head).forEach(v),b=p(t),q(o.$$.fragment,t),g=p(t),e=x(t,"DIV",{class:!0});var n=F(e);q(l.$$.fragment,n),$=p(n),d=x(n,"P",{"data-svelte-h":!0}),y(d)!=="svelte-1lq3l4c"&&(d.textContent=S),w=p(n),i=x(n,"H3",{class:!0,"data-svelte-h":!0}),y(i)!=="svelte-7briai"&&(i.textContent=A),k=p(n),a=x(n,"DIV",{id:!0,class:!0,"data-svelte-h":!0}),y(a)!=="svelte-101kg08"&&(a.textContent=B),j=p(n),s=x(n,"SCRIPT",{type:!0,id:!0,"data-svelte-h":!0}),y(s)!=="svelte-1e4b6ba"&&(s.textContent=I),n.forEach(v),this.h()},h(){h(i,"class","mb-4"),h(a,"id","HCB_comment_box"),h(a,"class","text-text svelte-1u5fl6j"),h(s,"type","text/javascript"),h(s,"id","hcb"),h(e,"class","max-w-content w-full")},m(t,c){E(t,b,c),H(o,t,c),E(t,g,c),E(t,e,c),H(l,e,null),r(e,$),r(e,d),r(e,w),r(e,i),r(e,k),r(e,a),r(e,j),r(e,s),f=!0},p(t,[c]){(!f||c&0)&&m!==(m="[ home@akross:/"+C+" ]")&&(document.title=m)},i(t){f||(G(o.$$.fragment,t),G(l.$$.fragment,t),f=!0)},o(t){T(o.$$.fragment,t),T(l.$$.fragment,t),f=!1},d(t){t&&(v(b),v(g),v(e)),D(o,t),D(l)}}}let C="guestbook";class L extends K{constructor(m){super(),M(this,m,null,Q,V,{})}}export{L as component};
