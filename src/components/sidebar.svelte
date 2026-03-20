2a0a2a64208bbbebd552a44139c7dffd8ccc0dc4
Akross <59298840+akrossu@users.noreply.github.com>
2026-03-19 22:07:14 -0400

    new style

--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -20,43 +20,53 @@
     };
 </script>
 
-<div class="text-center w-72 mr-16 hidden md:block min-w-fit" >
-    <div class="flex justify-center items-center mb-4">
-        <img class="w-24" src="/assets/iconcat.gif" alt="">
+<div class="w-72 mr-16 hidden md:block min-w-fit muted-border mica h-auto">
+    <div class="px-8 text-center ">
+        <div class="flex justify-center mb-4">
+            <img class="w-24" src="/assets/iconcat.gif" alt="">
+        </div>
+        <h2 class="mb-8 text-lg font-bold">website directory:</h2>
     </div>
-    <h2 class="mb-8 text-lg font-bold">website directory:</h2>
-    <ul class="mb-4 list-none">
-        <li><h3>\\\ ᨐ creative corner ///</h3></li>
-        <li><a href="/{pages.code}">[ /{pages.code} ]</a>{#if title == `${pages.code}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
-        <li>&gt coding projects</li>
-        <li><a href="/{pages.imageboard}">[ /{pages.imageboard} ]</a>{#if title == `${pages.imageboard}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
-        <li>&gt monthly photography</li>
-    </ul>
-    <ul class="mb-4 list-none">
-        <li><h3>\\\ ᨐ digital garden ///</h3></li>
-        <li><a href="/{pages.blog}">[ /{pages.blog} ]</a>{#if title == `${pages.blog}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
-        <li>&gt random writings</li>
-        <li><a href="/{pages.gamelist}">[ /{pages.gamelist} ]</a>{#if title == `${pages.gamelist}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
-        <li>&gt playing/completed games</li>
-        <li><a href="/{pages.musiclist}">[ /{pages.musiclist} ]</a>{#if title == `${pages.musiclist}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
-        <li>&gt favorite songs</li>
-    </ul>
-    <ul class="mb-4 list-none">
-        <li><h3>\\\ ᨐ information ///</h3></li>
-        <li><a href="/{pages.external}">[ /{pages.external} ]</a>{#if title == `${pages.external}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
-        <li>&gt cool websites</li>
-        <li><a href="/{pages.about}">[ /{pages.about} ]</a>{#if title == `${pages.about}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
-        <li>&gt about the webmaster</li>
-        <li><a href="/{pages.lore}">[ /{pages.lore} ]</a>{#if title == `${pages.lore}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
-        <li>&gt about this website</li>
-    </ul>
-    <ul class="mb-4 list-none">
-        <li><h3>\\\ ᨐ community ///</h3></li>
-        <li><a href="/{pages.webring}">[ /{pages.webring} ]</a>{#if title == `${pages.webring}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
-        <li>&gt web friends!</li>
-        <li><a href="/{pages.guestbook}">[ /{pages.guestbook} ]</a>{#if title == `${pages.guestbook}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
-        <li>&gt record your visit</li>
-        <li><a class="normal-case" href="https://www.discord.gg/sczXWkDsZ6" target="_blank">[ discord.gg/sczXWkDsZ6 ]</a></li>
-        <li>&gt community discord</li>
-    </ul>
+    <div class="px-8">
+        <ul class="mb-4 list-none">
+            <li><h3>\\\ ᨐ creative corner ///</h3></li>
+            <li><a href="/{pages.code}">[ /{pages.code} ]</a>{#if title == `${pages.code}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
+            <li aria-label="page description">&gt coding projects</li>
+            <li><a href="/{pages.imageboard}">[ /{pages.imageboard} ]</a>{#if title == `${pages.imageboard}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
+            <li aria-label="page description">&gt monthly photography</li>
+        </ul>
+        <ul class="mb-4 list-none">
+            <li><h3>\\\ ᨐ digital garden ///</h3></li>
+            <li><a href="/{pages.blog}">[ /{pages.blog} ]</a>{#if title == `${pages.blog}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
+            <li aria-label="page description">&gt random writings</li>
+            <li><a href="/{pages.gamelist}">[ /{pages.gamelist} ]</a>{#if title == `${pages.gamelist}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
+            <li aria-label="page description">&gt playing/completed games</li>
+            <li><a href="/{pages.musiclist}">[ /{pages.musiclist} ]</a>{#if title == `${pages.musiclist}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
+            <li aria-label="page description">&gt favorite songs</li>
+        </ul>
+        <ul class="mb-4 list-none">
+            <li><h3>\\\ ᨐ information ///</h3></li>
+            <li><a href="/{pages.external}">[ /{pages.external} ]</a>{#if title == `${pages.external}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
+            <li aria-label="page description">&gt cool websites</li>
+            <li><a href="/{pages.about}">[ /{pages.about} ]</a>{#if title == `${pages.about}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
+            <li aria-label="page description">&gt about the webmaster</li>
+            <li><a href="/{pages.lore}">[ /{pages.lore} ]</a>{#if title == `${pages.lore}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
+            <li aria-label="page description">&gt about this website</li>
+        </ul>
+        <ul class="mb-4 list-none">
+            <li><h3>\\\ ᨐ community ///</h3></li>
+            <li><a href="/{pages.webring}">[ /{pages.webring} ]</a>{#if title == `${pages.webring}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
+            <li aria-label="page description">&gt web friends!</li>
+            <li><a href="/{pages.guestbook}">[ /{pages.guestbook} ]</a>{#if title == `${pages.guestbook}`}<span class='inline-block'>&nbsp&lt---</span>{/if}</li>
+            <li aria-label="page description">&gt record your visit</li>
+            <li><a class="normal-case" href="https://www.discord.gg/sczXWkDsZ6" target="_blank">[ discord.gg/sczXWkDsZ6 ]</a></li>
+            <li aria-label="page description">&gt community discord</li>
+        </ul>
+    </div>
 </div>
+
+<style>
+    [aria-label="page description"] {
+        padding-inline-start: 1rem;
+    }
+</style>
