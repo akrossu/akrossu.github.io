e5129572cd3677edceff4a815c8888983853df1e
Akross <59298840+akrossu@users.noreply.github.com>
2026-06-29 14:03:21 -0400

    initial commit

---
+++ src/routes/camera-gear/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Header from '$components/header.svelte';
+    import Sidebar from '$components/sidebar.svelte';
+    import Button from '$components/Button.svelte';
+
+    const title = 'camera-gear';
+</script>
+
+<svelte:head>
+    <title>[ home@akross:/{title} ]</title>
+</svelte:head>
+<Sidebar title={title}></Sidebar>
+
+<div class="max-w-content w-full">
+    <Header title={title}></Header>
+    <span>here's my content</span>
+</div>
