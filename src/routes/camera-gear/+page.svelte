796c6fee49ca52d00e59c060cca2a9b8251b1a26
Akross <59298840+akrossu@users.noreply.github.com>
2026-06-29 22:27:46 -0400

    added camera gear

--- src/routes/camera-gear/+page.svelte
+++ src/routes/camera-gear/+page.svelte
@@ -13,5 +13,15 @@
 
 <div class="max-w-content w-full">
     <Header title={title}></Header>
-    <span>i haven't added anything here yet :(</span>
+    <p>&gt a small list of what i shoot with daily</p>
+    <br>
+    <h2>camera:</h2>
+    <p>&gt Fujifilm xs-20</p>
+    <p><a href="https://shopusa.fujifilm-x.com/x-s20-x-s20/">[ external:/fujifilm ]</a></p>
+    <br>
+    <h2>lenses:</h2>
+    <p>&gt Fujifilm xf 16-50mm f2.8-4.8 r lm wr variable zoom lens</p>
+    <a href="https://www.fujifilm-x.com/en-us/products/lenses/xf16-50mmf28-48-r-lm-wr/">[ external:/fujifilm ]</a>
+    <p>&gt ttartisan 23mm f1.4 prime lens</p>
+    <a href="https://ttartisan.com/?aps-c-lenses/76.html">[ external:/ttartisan ]</a>
 </div>
